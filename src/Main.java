3405730cf00811cbf2fc015c69cde38707be8111
eccentric <cool.eccentric@yandex.ru>
2024-09-09 17:15:50 +0300

    mend

--- src/Main.java
+++ src/Main.java
@@ -5,61 +5,45 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] arr = {1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1};
+        Integer[] arr = {1, 1, 0, 1};
         System.out.println(countElementsArr(arr));
 
     }
 
     public static int countElementsArr(Integer[] arr) {
 
-        List<Integer> list = Arrays.asList(arr.clone());
-        List<Integer> list_copy = new ArrayList<>(list);
+        int totalOnes = 0;
 
-        int local0 = 0;
-        int global0 = 0;
-
-        int local1 = 0;
-        int global1 = 0;
-
-        int index = 0;
-        int indexForRemove = 0;
-
-        for (Integer i : list_copy) {
-            if (i == 1) {
-                local1++;
-                if (local0 <= global0) {
-                    global0 = local0;
-                }
-                local0 = 0;
-            } else {
-                local0++;
-                if (local1 > global1) {
-                    global1 = local1;
-                }
-                local1 = 0;
-                indexForRemove = index;
-            }
-            index++;
+        for (int i : arr) {
+            if (i == 1)
+            totalOnes++;
+        }
+        if (totalOnes == arr.length) {
+            return totalOnes;
         }
 
-        list_copy.remove(indexForRemove);
+        int currentOnes = 0;
+        int maxOnes = 0;
 
-        int local_max = 0;
-        int global_max = 0;
 
-        for (Integer i : list_copy) {
-            if (i == 1) {
-                local_max++;
-                if (local_max > global_max) {
-                    global_max = local_max;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                currentOnes++;
+            } else if (arr[i] == 0) {
+                int leftOnes = currentOnes;
+                int rightOnes = 0;
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] == 1) {
+                        rightOnes++;
+                    } else {
+                        break;
+                    }
                 }
-            } else {
-                local_max = 0;
+                maxOnes = Math.max(maxOnes, leftOnes + rightOnes);
+                currentOnes = 0;
             }
         }
-
-
-        return global_max;
+        return maxOnes;
     }
 }
 
